1ae2961fcdc57de49d6c48ec2cee1c029bde16cc
root <benoit.beti@gmail.com>
2024-02-20 13:22:04 +0000

    commit task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	a = 'a';
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	putchar (a);
+	putchar ('\n');
+
+	return (0);
+}
