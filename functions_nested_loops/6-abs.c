38977613652987f9eae98fe8f44a45175a2b6bf9
BenBet21 <benoit.beti.com>
2024-02-22 14:40:06 +0000

    commit task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @n: character to be verify the absolue value
+ *
+ * Return: absolue value of n
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{	return (-n);
+	}
+
+}
