998a1959212585e98f24db1ce45b36c1c6932120
BenBet21 <benoit.beti@gmail.com>
2024-02-23 15:51:43 +0000

    commit task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @x: character to verify
+ * @y: character to verify
+ * Return: value of x+y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+		}
+
+
