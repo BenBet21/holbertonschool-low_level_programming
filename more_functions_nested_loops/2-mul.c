0ee665603cdb8136fe3577ea288013e436be95e7
BenBet21 <benoit.beti@gmail.com>
2024-02-26 10:30:26 +0000

    commit task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: character to be verify
+ * @b: characteur to be verify
+ * Return: always (0)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
