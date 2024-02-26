fc270e11b9a4e3fba2ca0de2da30b70e42a30bdf
BenBet21 <benoit.beti@gmail.com>
2024-02-26 15:19:10 +0000

    commit task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: character to be verify
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0 ; a < n ; a++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+
+}
