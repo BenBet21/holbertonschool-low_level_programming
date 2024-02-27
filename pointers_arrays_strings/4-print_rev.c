0996f57b0fa09585c6cf42d1236ffa90b5ac2b68
BenBet21 <benoit.beti@gmail.com>
2024-02-27 21:35:13 +0000

    commit task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,16 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = (*s - 7) ; a >= -1 ; --a)
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	a = a - 1;
+	while (a >= 0)
 	{
 		_putchar(s[a]);
+		a--;
 	}
 	_putchar('\n');
 }
