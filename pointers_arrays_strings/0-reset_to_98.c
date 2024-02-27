170cb5f8d833ee0cdf16d0dfd46491c37bab6c61
BenBet21 <benoit.beti@gmail.com>
2024-02-27 13:01:31 +0000

    commit task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ * @n: number to be verify
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+New project "C - Pointers, arrays and strings"" week 4 Tuesday  27th of February
