7f262496e9735a391a14e51a6b813d6791fddccb
BenBet21 <benoit.beti@gmail.com>
2024-02-29 11:43:46 +0100

    commit task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,37 +1,35 @@
 #include "main.h"
 
 /**
- * puts_half - entry point
- * @len: pour avoir la longeur de la chaine
- * @i: pour le compteur
- * 
- * */
+ * puts2  - entry point
+ * @str: designe la chaine de caractères
+ */
 
-void puts_half(char *str)
+void puts2(char *str)
 {
-    int len = 0;
-    int i = 0;
+	int len = 0;
+	int i = 0;
 
-    while (str[len])
-    {
-        len++;
-    }
-    
-    
-    if (len % 2 == 0)
-    {
-    for (i = len / 2 ; i <= len ; i++)
-    {
-      _putchar(str[i]);
-    }
-    }
-    
-    else
-    {
-    for (i = (len -1) / 2 ; i <= len ; i++)
-    {
-      _putchar(str[i]);
-    }
-    }
-    _putchar('\n');
+	while (str[len])
+	{
+		len++;
+	}
+
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2 ; i <= len ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	else
+	{
+		for (i = (len - 1) / 2 ; i <= len ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
