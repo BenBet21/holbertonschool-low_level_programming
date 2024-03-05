3f98b0ef4db4203f17538b4539763f10ba7d49ea
BenBet21 <benoit.beti@gmail.com>
2024-03-05 11:58:40 +0100

    commit task7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,15 +12,18 @@ void print_diagsums(int *a, int size)
 {
 	unsigned int somme1, somme2;
 	int i, nbcase;
+	nbcase = 0;
+	somme1 = 0;
+	somme2 = 0;
 
 	nbcase = (size * size) - 1;
 
-	for (i = 0 ; i <= nbcase ; i = i + size + 1)
+	for (i = 0 ; i <= nbcase ; i = i + (size + 1))
 	{
 		somme1 = somme1 + a[i];
 	}
 
-	for (i = size - 1 ; i < nbcase ; i = i + (size - 1))
+	for (i = (size - 1) ; i < nbcase ; i = i + (size - 1))
 	{
 		somme2 = somme2 + a[i];
 	}
