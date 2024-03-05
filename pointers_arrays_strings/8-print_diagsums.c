30b00338531a0e8da03c9d8fe65ca6df7b73dcc5
BenBet21 <benoit.beti@gmail.com>
2024-03-05 11:54:26 +0100

    commit task7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - entry point
+ * @a: variable
+ * @size : taille de la matrice carré
+ */
+
+void print_diagsums(int *a, int size)
+
+{
+	unsigned int somme1, somme2;
+	int i, nbcase;
+
+	nbcase = (size * size) - 1;
+
+	for (i = 0 ; i <= nbcase ; i = i + size + 1)
+	{
+		somme1 = somme1 + a[i];
+	}
+
+	for (i = size - 1 ; i < nbcase ; i = i + (size - 1))
+	{
+		somme2 = somme2 + a[i];
+	}
+
+	printf("%d, %d\n", somme1, somme2);
+
+}
