96d955e5f09c1ab174a8e943aa59b713c18495fe
BenBet21 <benoit.beti@gmail.com>
2024-03-05 10:01:27 +0100

    commit task6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - entry point
+ * @a: variable
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, j;
+	int k = 0;
+
+	while (*a[k])
+	{
+		k++;
+	}
+
+	for (i = 0 ; i < k - 1 ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+
+		if (a[i][j] >= 65 && a[i][j] <= 90)
+		{
+			_putchar(a[i][j]);
+		}
+		if (a[i][j] >= 97 && a[i][j] <= 122)
+		{
+			_putchar(a[i][j]);
+		}
+		}
+		putchar('\n');
+	}
+
+}
