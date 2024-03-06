e4b1021ef508cd23e47310365c547870f04e6948
BenBet21 <benoit.beti@gmail.com>
2024-03-06 15:14:41 +0100

    commit task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
