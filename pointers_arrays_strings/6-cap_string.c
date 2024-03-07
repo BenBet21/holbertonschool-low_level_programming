b7812f6ff2ab6acfb50ed4aef2faefeca6b09342
BenBet21 <benoit.beti@gmail.com>
2024-03-07 14:37:29 +0100

    commit task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 {
 	int i = 0;
 	int j = 0;
-	char separateurs[] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+	char separateurs[50] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}', '\0'};
 
 	if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
