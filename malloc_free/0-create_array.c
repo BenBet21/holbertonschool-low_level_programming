f4d53e650b59d44151e776a62ee9fab1f1a1e64c
BenBet21 <benoit.beti@gmail.com>
2024-03-11 12:46:39 +0100

    commit task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - entry point
+ * @size: variable
+ * @c: variable
+ * Return: NULL if size = 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc((size) * sizeof(*str));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+
+	str[i] = '\0';
+	return (str);
+
+}
+
+
+
