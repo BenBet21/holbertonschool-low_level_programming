9fb6a083e4571585a27fc85498d2aae9525fba56
BenBet21 <benoit.beti@gmail.com>
2024-03-12 06:46:39 +0100

    commit task2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * taille - entry point
+ * @str: variable
+ * Return: NULL ou taille de STR
+ */
+
+
+int taille(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+	i++;
+	}
+	return (i);
+}
+
+/**
+ * *str_concat - entry point
+ * @s1: variable
+ * @s2: variable
+ * Return: different values
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i;
+	int size1;
+	int size2;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	size1 = taille(s1);
+	size2 = taille(s2);
+
+	concat = malloc((size1 + size2 + 1) * sizeof(char));
+
+	if (concat == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0 ; i < size1 ; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (i = 0 ; i < size2; i++)
+		{
+		concat[size1 + i] = s2[i];
+		}
+	concat[size1 + size2] = '\0';
+
+	return (concat);
+}
