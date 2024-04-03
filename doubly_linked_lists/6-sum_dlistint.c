29b3e19d7b2f0b6b0f049cfe3f956c93b4c55823
BenBet21 <benoit.beti@gmail.com>
2024-04-03 21:03:04 +0200

    Write a function that returns the sum of all the data (n) of a dlistint_t linked list.Task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - this function sums all the date (n)
+ * of a dlistint_t linked list.
+ * @head: a pointer to the head of the list
+ * Return: sul of all date
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int somme = 0;
+
+	while (head != NULL)
+	{
+		somme += head->n;
+		head = head->next;
+	}
+	return (somme);
+}
