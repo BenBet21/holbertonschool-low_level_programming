1c7db551e29848d876555e9e77d2ed55482bef62
BenBet21 <benoit.beti@gmail.com>
2024-04-05 11:36:03 +0200

    Write a program that copies the content of a file to another file. Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,48 +11,48 @@
  * @argv: Array of arguments
  * Return: 0 on success, otherwise exit status
  */
+
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
 	char buf[BUF_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		return (99);
 	}
 	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buf, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
+		if (bytes_written != bytes_read)
 		{
-			dprintf(2, "Error: Can't write to file %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			return (99);
 		}
 	}
 	if (bytes_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
 	}
-	close(fd_from);
-	close(fd_to);
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		return (100);
+	}
 	return (0);
 }
